a4243b1971f99e35cfcd90c04aff8f1319359d0c
Marc DELOMEZ <marc.delomez@free.fr>
2018-08-15 14:07:10 +0200

    fix bug in photo webapp

--- front/src/webapps/photo/main.js
+++ front/src/webapps/photo/main.js
@@ -10,20 +10,22 @@ $$.registerControl('MainControl', ['FileService'], function(elt, fileSrv) {
 		},
 		events: {
 			onFileClicked: function(ev, data) {
-				//console.log('onFileClicked', data)
+				console.log('onFileClicked', data)
 				var files = $(this).interface().getFiles()
-				//console.log('files', files)
+				console.log('files', files)
 
-				var images = files
-				.filter((f) => !f.isDir)
-				.map((f) => fileSrv.fileUrl(data.rootDir + f.name))
-				//console.log('images', images)
+				var images = files.filter((f) => !f.isDir)
+
+				var index = images.findIndex((f) => f.name == data.name)
+
+				var imagesUrl = images.map((f) => fileSrv.fileUrl(data.rootDir + f.name))
+				console.log('imagesUrl', imagesUrl)
 				
 
-				var index = files.findIndex((f) => f.name == data.name)
+				
 				//console.log('index', index)
 
-				ctrl.setData({images, index, showFiles: false})
+				ctrl.setData({images: imagesUrl, index, showFiles: false})
 			},
 
 			onBackClicked: function() {
