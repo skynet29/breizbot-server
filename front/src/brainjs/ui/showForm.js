8c04901ec91b5f5ce66d64043ba1ce1bcb7a85f6
Marc DELOMEZ <marc.delomez@free.fr>
2018-09-07 18:05:57 +0200

    add brainjs showForm utility function

---
+++ front/src/brainjs/ui/showForm.js
@@ -0,0 +1,53 @@
+$$.showForm = function(formDesc, data, onApply) {
+	//console.log('showForm', formDesc)
+
+	var div = $('<div>', {title: formDesc.title})
+
+	var form = $('<form>')
+		.appendTo(div)
+		.on('submit', function(ev) {
+			ev.preventDefault()
+			div.dialog('close')
+			if (typeof onApply == 'function') {
+				onApply(form.getFormData())
+			}				
+		})
+	var submitBtn = $('<input>', {type: 'submit', hidden: true}).appendTo(form)
+
+	const fieldsDesc = formDesc.fields
+	for(let fieldName in fieldsDesc) {
+		const fieldDesc = fieldsDesc[fieldName]
+
+		const {label, input, value, attrs} = fieldDesc
+
+		const divField = $('<div>', {class: 'bn-flex-row bn-space-between'}).appendTo(form)
+		$('<label>').text(label).appendTo(divField)
+
+		if (input === 'input') {
+			$('<input>')
+				.width(100)
+				.attr(attrs)
+				.attr('name', fieldName)
+				.val(value)
+				.prop('required', true)
+				.appendTo(divField)
+		}
+	}
+
+	div.dialog({
+		modal: true,
+		//width: 'auto',
+		close: function() {
+			$(this).dialog('destroy')
+		},
+		buttons: {
+			'Cancel': function() {
+				$(this).dialog('close')
+			},
+			'Apply': function() {					
+				submitBtn.click()
+			}
+		}
+	})
+
+};
